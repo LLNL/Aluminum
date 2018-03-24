11a82117515b270597cf1651bef0041181a20e45
Nikoli Dryden <dryden1@llnl.gov>
2018-03-24 09:53:55 -0700

    Clean up, slightly reorganize, the NCCL backend.

--- src/allreduce_nccl_impl.cpp
+++ src/allreduce_nccl_impl.cpp
@@ -27,21 +27,19 @@ NCCLCommunicator::NCCLCommunicator(MPI_Comm comm_, std::vector<int> gpus)
   : MPICommunicator(comm_),
     m_gpus(gpus),
     m_num_gpus(gpus.size()) {
-  MPI_Comm_dup(comm_, &mpi_comm);
   gpu_setup();
   nccl_setup();
 }
 
 NCCLCommunicator::~NCCLCommunicator() {
   nccl_destroy();
-  for (size_t i=0; i<m_gpus.size(); ++i) {
+  for (size_t i = 0; i < m_gpus.size(); ++i) {
     CUDACHECK(cudaSetDevice(m_gpus[i]));
     CUDACHECK(cudaStreamDestroy(m_streams[i]));
   }
 }
 
 void NCCLCommunicator::gpu_setup() {
-
   // Initialize list of GPUs
   if (m_gpus.empty()) {
     m_gpus.push_back(0);
@@ -55,14 +53,14 @@ void NCCLCommunicator::gpu_setup() {
     m_streams.push_back(nullptr);
     CUDACHECK(cudaStreamCreate(&m_streams.back()));
   }
-
 }
 
 void NCCLCommunicator::nccl_setup() {
-
-  if(m_num_gpus != 1){
-    std::cerr << "NCCLCommunicator: rank " << rank() << ": the number of GPUs assigned to process is " << m_num_gpus << "; should be 1\n";
-    MPI_Abort(mpi_comm, -3);
+  if (m_num_gpus != 1) {
+    std::cerr << "NCCLCommunicator: rank " << rank()
+              << ": the number of GPUs assigned to process is " << m_num_gpus
+              << "; should be 1" << std::endl;
+    MPI_Abort(get_comm(), -3);
   }
 
   int num_gpus_assigned = m_num_gpus;
@@ -75,34 +73,20 @@ void NCCLCommunicator::nccl_setup() {
     NCCLCHECK(ncclGetUniqueId(&ncclId));
   }
 
-  MPI_Bcast(&ncclId, sizeof(ncclId), MPI_BYTE, 0, mpi_comm);
+  MPI_Bcast(&ncclId, sizeof(ncclId), MPI_BYTE, 0, get_comm());
 
   if (nProcs == 1) {
     int gpuArray = 0;
     NCCLCHECK(ncclCommInitAll(&m_nccl_comm, 1, &gpuArray));
   }
   else {
-    NCCLCHECK(ncclCommInitRank(&m_nccl_comm, total_num_comms, ncclId, num_gpus_assigned*myid));
-/*
-    if(num_gpus_assigned > 1) NCCLCHECK(ncclGroupStart());
-    for(int i=0; i<num_gpus_assigned; i++){
-      CUDACHECK(cudaSetDevice(m_gpus[i]));
-      NCCLCHECK(ncclCommInitRank(&(m_nccl_comm[i]), total_num_comms, ncclId, num_gpus_assigned*myid+i));
-    }
-    if(num_gpus_assigned > 1) NCCLCHECK(ncclGroupEnd());
-*/
+    NCCLCHECK(ncclCommInitRank(&m_nccl_comm, total_num_comms, ncclId,
+                               num_gpus_assigned*myid));
   }
-} // nccl_setup
+}
 
 void NCCLCommunicator::nccl_destroy() {
   ncclCommDestroy(m_nccl_comm);
-/*
-  int num_gpus_assigned = m_num_gpus;
-  synchronize();
-  for(int i=0; i<num_gpus_assigned; i++){
-    ncclCommDestroy(m_nccl_comm[i]);
-  }
-*/
 }
 
 void NCCLCommunicator::synchronize() {
@@ -116,94 +100,48 @@ cudaStream_t NCCLCommunicator::get_default_stream() {
   return m_streams[0];
 }
 
-/// It is assumed that both sendbuf and recvbuf are in device memory
-/// for NCCL sendbuf and recvbuf can be identical; in-place operation will be performed
-void NCCLCommunicator::Allreduce(void* sendbuf, void* recvbuf, size_t count, ncclDataType_t nccl_type,
-               ncclRedOp_t nccl_redop, cudaStream_t default_stream) {
-
-  if(count == 0) return;
-
-  NCCLCHECK(ncclAllReduce(sendbuf, recvbuf, count, nccl_type, nccl_redop, m_nccl_comm, default_stream));
-
-/*
-  int num_gpus_assigned = m_gpus.size();
-
-  if(num_gpus_assigned > 1) ncclGroupStart();
-  for(int i = 0; i < num_gpus_assigned; ++i) {
-    CUDACHECK(cudaSetDevice(m_gpus[i]));
-    NCCLCHECK(ncclAllReduce(sendbuf, recvbuf, count, nccl_type, nccl_redop, m_nccl_comm[i], m_streams[i]));
-  }
-  if(num_gpus_assigned > 1) ncclGroupEnd();
-*/
-
+void NCCLCommunicator::Allreduce(void* sendbuf, void* recvbuf, size_t count,
+                                 ncclDataType_t nccl_type,
+                                 ncclRedOp_t nccl_redop,
+                                 cudaStream_t default_stream) {
+  if (count == 0) return;
+  NCCLCHECK(ncclAllReduce(sendbuf, recvbuf, count, nccl_type, nccl_redop,
+                          m_nccl_comm, default_stream));
 }
 
-
-/// It is assumed that both sendbuf and recvbuf are in device memory
-/// for NCCL sendbuf and recvbuf can be identical; in-place operation will be performed
-void NCCLCommunicator::Reduce(void* sendbuf, void* recvbuf, size_t count, ncclDataType_t nccl_type,
-               ncclRedOp_t nccl_redop, int root, cudaStream_t default_stream) {
-
-  if(count == 0) return;
-
-  NCCLCHECK(ncclReduce(sendbuf, recvbuf, count, nccl_type, nccl_redop, root, m_nccl_comm, default_stream));
-
-/*
-  int num_gpus_assigned = m_gpus.size();
-
-  if(num_gpus_assigned > 1) ncclGroupStart();
-  for(int i = 0; i < num_gpus_assigned; ++i) {
-    CUDACHECK(cudaSetDevice(m_gpus[i]));
-    NCCLCHECK(ncclAllReduce(sendbuf, recvbuf, count, nccl_type, nccl_redop, m_nccl_comm[i], m_streams[i]));
-  }
-  if(num_gpus_assigned > 1) ncclGroupEnd();
-*/
-
+void NCCLCommunicator::Reduce(void* sendbuf, void* recvbuf, size_t count,
+                              ncclDataType_t nccl_type, ncclRedOp_t nccl_redop,
+                              int root, cudaStream_t default_stream) {
+  if (count == 0) return;
+  NCCLCHECK(ncclReduce(sendbuf, recvbuf, count, nccl_type, nccl_redop, root,
+                       m_nccl_comm, default_stream));
 }
 
-void NCCLCommunicator::Bcast(void* sendbuf, size_t count, ncclDataType_t nccl_type, int root, cudaStream_t default_stream) {
-
-  if(count == 0) return;
-  NCCLCHECK(ncclBcast(sendbuf, count, nccl_type, root, m_nccl_comm, default_stream));
-
-/*
-  int num_gpus_assigned = m_gpus.size();
-
-  if(num_gpus_assigned > 1) ncclGroupStart();
-  for(int i = 0; i < num_gpus_assigned; ++i) {
-    CUDACHECK(cudaSetDevice(m_gpus[i]));
-    NCCLCHECK(ncclAllReduce(sendbuf, recvbuf, count, nccl_type, nccl_redop, m_nccl_comm[i], m_streams[i]));
-  }
-  if(num_gpus_assigned > 1) ncclGroupEnd();
-*/
-
+void NCCLCommunicator::Bcast(void* sendbuf, size_t count,
+                             ncclDataType_t nccl_type, int root,
+                             cudaStream_t default_stream) {
+  if (count == 0) return;
+  NCCLCHECK(ncclBcast(sendbuf, count, nccl_type, root, m_nccl_comm,
+                      default_stream));
 }
 
-/**
- * It is assumed for NCCL-based Allgather that
- *   1. send and recv buffers have the same type
- *   2. the recv_count is computed implicitly by NCCL
- *   3. recv buffer should have a size of at least num_ranks*send_count
-*/
-void NCCLCommunicator::Allgather(void* sendbuf, void* recvbuf, size_t send_count,
-                 ncclDataType_t nccl_type, cudaStream_t default_stream) {
-
-  if(send_count == 0) return;
+void NCCLCommunicator::Allgather(void* sendbuf, void* recvbuf,
+                                 size_t send_count, ncclDataType_t nccl_type,
+                                 cudaStream_t default_stream) {
+  if (send_count == 0) return;
   NCCLCHECK(ncclAllGather(sendbuf, recvbuf, send_count, nccl_type, m_nccl_comm, default_stream));
 }
 
-/**
- * It is assumed that both sendbuf and recvbuf are in device memory
- * For NCCL sendbuf and recvbuf can be identical; in-place operation will be performed
- * NCCL-based Reduce_scatter assumes that send_count is equal to num_ranks*recv_count, which means
- * that send_buf should have a size of at least num_ranks*recv_count elements
-*/
-void NCCLCommunicator::Reduce_scatter(void* sendbuf, void* recvbuf, size_t recv_count, ncclDataType_t nccl_type,
-                 ncclRedOp_t nccl_redop, cudaStream_t default_stream) {
-
-  if(recv_count == 0) return;
-  NCCLCHECK(ncclReduceScatter(sendbuf, recvbuf, recv_count, nccl_type, nccl_redop, m_nccl_comm, default_stream));
+void NCCLCommunicator::Reduce_scatter(void* sendbuf, void* recvbuf,
+                                      size_t recv_count,
+                                      ncclDataType_t nccl_type,
+                                      ncclRedOp_t nccl_redop,
+                                      cudaStream_t default_stream) {
+  if (recv_count == 0) return;
+  NCCLCHECK(ncclReduceScatter(sendbuf, recvbuf, recv_count, nccl_type,
+                              nccl_redop, m_nccl_comm, default_stream));
 }
 
-} // namespace allreduces
-#endif
+}  // namespace allreduces
+
+#endif  // ALUMINUM_HAS_NCCL
--- src/allreduce_nccl_impl.hpp
+++ src/allreduce_nccl_impl.hpp
@@ -19,76 +19,147 @@ inline std::string allreduce_name(NCCLCollectiveAlgorithm algo) {
   }
 }
 
-/// We assume NCCL version 2.0 or higher for allreduce to work
+/**
+ * Communicator for NCCL-based allreduces.
+ * This requires NCCL version 2.0 or higher.
+ * This currently requires there to be only one GPU per MPI rank.
+ */
 class NCCLCommunicator : public MPICommunicator {
  public:
+  /**
+   * Initialize a NCCL communicator.
+   * @param comm_ An MPI_Comm representing the nodes to be in the communicator.
+   * @param gpus The GPUs this communicator is (locally) managing.
+   */
   NCCLCommunicator(MPI_Comm comm_ = MPI_COMM_WORLD,
                    std::vector<int> gpus = std::vector<int>());
-
   ~NCCLCommunicator() override;
+  Communicator* copy() const override {
+    return new NCCLCommunicator(get_comm(), m_gpus);
+  }
 
-  Communicator* copy() const override { return new NCCLCommunicator(mpi_comm); }
-
+  /** Synchronize the internal stream for each managed GPU. */
   void synchronize();
 
+  /** Return the default stream for this communicator. */
   cudaStream_t get_default_stream();
 
-  /// For following reduction functions, it is assumed that both sendbuf and recvbuf are in device memory
-  /// for NCCL sendbuf and recvbuf can be identical; in-place operation will be performed
-  void Allreduce(void* sendbuf, void* recvbuf, size_t count, ncclDataType_t nccl_type,
-               ncclRedOp_t nccl_redop, cudaStream_t default_stream); 
-
-  void Reduce(void* sendbuf, void* recvbuf, size_t count, ncclDataType_t nccl_type,
-               ncclRedOp_t nccl_redop, int root, cudaStream_t default_stream); 
-
-  /// It is assumed that the sendbuff is in device memory for broadcast operation
-  void Bcast(void* sendbuf, size_t count, ncclDataType_t nccl_type, int root, 
-               cudaStream_t default_stream); 
+  /**
+   * Perform an allreduce on data in device memory.
+   * @param sendbuf Input data.
+   * @param recvbuf Output data; if same as sendbuf, an in-place allreduce is done.
+   * @param count Number of elements in sendbuf/recvbuf.
+   * @param nccl_type Type of data being reduced.
+   * @param nccl_redop The reduction operation to perform.
+   * @param default_stream CUDA stream to associate with the operation.
+   */
+  void Allreduce(void* sendbuf, void* recvbuf, size_t count,
+                 ncclDataType_t nccl_type, ncclRedOp_t nccl_redop,
+                 cudaStream_t default_stream); 
 
   /**
-  * It is assumed that both sendbuf and recvbuf are in device memory
-  * For NCCL sendbuf and recvbuf can be identical; in-place operation will be performed
-  * NCCL-based Allgather assumes that recv_count is equal to num_ranks*send_count, which means
-  * that recv_buf should have a size of at least num_ranks*send_count elements
-  */
-  void Allgather(void* sendbuf, void* recvbuf, size_t count,
+   * Perform a reduce on data in device memory.
+   * @param sendbuf Input data.
+   * @param recvbuf Output data; if same as sendbuf, an in-place reduce is done.
+   * @param count Number of elements in sendbuf/recvbuf.
+   * @param nccl_type Type of data being reduced.
+   * @param nccl_redop The reduction operation to perform.
+   * @param root Rank to receive the final result.
+   * @param default_stream CUDA stream to associate with the operation.
+   */
+  void Reduce(void* sendbuf, void* recvbuf, size_t count,
+              ncclDataType_t nccl_type, ncclRedOp_t nccl_redop, int root,
+              cudaStream_t default_stream); 
+
+  /**
+   * Perform a broadcast on data in device memory.
+   * @param buf Data to send on the root; receive buffer on other ranks.
+   * @param count Number of elements in buf.
+   * @param nccl_type Type of data being broadcast.
+   * @param root Rank initiating the broadcast.
+   * @param default_stream CUDA stream to associate with the operation.
+   */
+  void Bcast(void* buf, size_t count, ncclDataType_t nccl_type, int root, 
+             cudaStream_t default_stream); 
+
+  /**
+   * Perform an allgather on data in device memory.
+   * @param sendbuf Input data; if this is an offset into recvbuf, an in-place
+   * operation is performed.
+   * @param recvbuf Output data; of size count*ranks.
+   * @param send_count Number of elements to send.
+   * @param nccl_type Type of data being gathered.
+   * @param default_stream CUDA stream to associate with the operation.
+   */
+  void Allgather(void* sendbuf, void* recvbuf, size_t send_count,
                  ncclDataType_t nccl_type, cudaStream_t default_stream);
 
   /**
-  * It is assumed that both sendbuf and recvbuf are in device memory
-  * For NCCL sendbuf and recvbuf can be identical; in-place operation will be performed
-  * NCCL-based Reduce_scatter assumes that send_count is equal to num_ranks*recv_count, which means
-  * that send_buf should have a size of at least num_ranks*recv_count elements
-  */
-  void Reduce_scatter(void* sendbuf, void* recvbuf, size_t recv_count, ncclDataType_t nccl_type,
-                     ncclRedOp_t nccl_redop, cudaStream_t default_stream);
+   * Perform a reduce-scatter on data in device memory.
+   * @param sendbuf Input data; of size recv_count*ranks.
+   * @param recvbuf Output data; if this is an offset into sendbuf, an in-place
+   * operation is performed.
+   * @param recv_count Number of elements to receive.
+   * @param nccl_type Type of data being gathered.
+   * @param nccl_redop The reduction operation to perform.
+   * @param default_stream CUDA stream to associate with the operation.
+   */
+  void Reduce_scatter(void* sendbuf, void* recvbuf, size_t recv_count,
+                      ncclDataType_t nccl_type, ncclRedOp_t nccl_redop,
+                      cudaStream_t default_stream);
 
-
-protected:
-
+ protected:
+  /** Initialize GPU information. */
   void gpu_setup();
-
+  /** Initialize the internal NCCL communicator. */
   void nccl_setup();
-
+  /** Clean up NCCL. */
   void nccl_destroy();
 
  private:
-
-  MPI_Comm mpi_comm;
-
-  /** List of GPU related variables. */
-  /// List of GPUs to be used
+  /** List of GPUs associated with this communicator. */
   std::vector<int> m_gpus;
-  /// List of CUDA streams
+  /** List of streams, one for each GPU. */
   std::vector<cudaStream_t> m_streams;
-  /// Number of GPUs allocated to the current rank
+  /** GPUs allocated to this rank. */
   int m_num_gpus;
-
-  /** List of NCCL 2 related variables. */
-  /// NOTE: It is assumed that ONLY ONE GPU is allocated to one MPI rank
+  /** NCCL communicator. */
   ncclComm_t m_nccl_comm;
 };
 
+namespace internal {
+namespace nccl {
+/** Convert a ReductionOperator to the corresponding ncclRedOp_t. */
+inline ncclRedOp_t ReductionOperator2ncclRedOp(ReductionOperator op) {
+  switch(op) {
+  case ReductionOperator::sum:
+    return ncclSum;
+  case ReductionOperator::prod:
+    return ncclProd;
+  case ReductionOperator::min:
+    return ncclMin;
+  case ReductionOperator::max:
+    return ncclMax;
+  default:
+    throw_allreduce_exception("Reduction operator not supported");
+  }
+}
+template <typename T>
+inline ncclDataType_t TypeMap();
+template <> inline ncclDataType_t TypeMap<char>() { return ncclChar; }
+template <> inline ncclDataType_t TypeMap<unsigned char>() { return ncclUint8; }
+template <> inline ncclDataType_t TypeMap<int>() { return ncclInt; }
+template <> inline ncclDataType_t TypeMap<unsigned int>() { return ncclUint32; }
+template <> inline ncclDataType_t TypeMap<long long int>() { return ncclInt64; }
+template <> inline ncclDataType_t TypeMap<unsigned long long int>() { return ncclUint64; }
+// TODO half-precision.
+//template <> inline ncclDataType_t TypeMap<??>() { return ncclHalf; }
+template <> inline ncclDataType_t TypeMap<float>() { return ncclFloat; }
+template <> inline ncclDataType_t TypeMap<double>() { return ncclDouble; }
+}  // namespace nccl
+}  // namespace internal
+
+/** Backend implementing NCCL communication. */
 class NCCLBackend {
  public:
   using algo_type = NCCLCollectiveAlgorithm;
@@ -114,59 +185,22 @@ class NCCLBackend {
   }
 
   template <typename T>
-  static void NonblockingAllreduce(
-      const T* sendbuf, T* recvbuf, size_t count,
-      ReductionOperator op,
-      comm_type& comm,
-      req_type& req,
-      algo_type algo) {
-    ncclDataType_t nccl_type;
-    switch(sizeof(T)) {
-      case 8:
-        nccl_type = ncclDouble;
-        break;
-      case 4:
-        nccl_type = ncclFloat;
-        break;
-      case 2:
-        nccl_type = ncclHalf;
-        break;
-      default:
-        throw_allreduce_exception("unsupported NCCL data type");
-    }
-
-    ncclRedOp_t nccl_redop;
-    switch(op) {
-      case ReductionOperator::sum:
-        nccl_redop = ncclSum;
-        break;
-      case ReductionOperator::prod:
-        nccl_redop = ncclProd;
-        break;
-      case ReductionOperator::min:
-        nccl_redop = ncclMin;
-        break;
-      case ReductionOperator::max:
-        nccl_redop = ncclMax;
-        break;
-      default:
-        throw_allreduce_exception("unsupported NCCL reduction operator");
-    }
-
+  static void NonblockingAllreduce(const T* sendbuf, T* recvbuf, size_t count,
+                                   ReductionOperator op, comm_type& comm,
+                                   req_type& req, algo_type) {
+    ncclDataType_t nccl_type = internal::nccl::TypeMap<T>();
+    ncclRedOp_t nccl_redop = internal::nccl::ReductionOperator2ncclRedOp(op);
     if (sendbuf == internal::IN_PLACE<T>()) {
       sendbuf = recvbuf;
     }
-    
     comm.Allreduce((void*) sendbuf, (void*) recvbuf, count,
                    nccl_type, nccl_redop, req);
   }
 
   template <typename T>
-  static void NonblockingAllreduce(
-      T* recvbuf, size_t count,
-      ReductionOperator op, comm_type& comm,
-      req_type& req,
-      algo_type algo) {
+  static void NonblockingAllreduce(T* recvbuf, size_t count,
+                                   ReductionOperator op, comm_type& comm,
+                                   req_type& req, algo_type algo) {
     NonblockingAllreduce(internal::IN_PLACE<T>(), recvbuf, count, op, comm,
                          req, algo);
   }
@@ -189,67 +223,25 @@ class NCCLBackend {
   }
 
   template <typename T>
-  static void NonblockingReduce(
-      const T* sendbuf, T* recvbuf, size_t count,
-      ReductionOperator op,
-      int root,
-      comm_type& comm,
-      req_type& req,
-      algo_type algo) {
-
-    ncclDataType_t nccl_type;
-    switch(sizeof(T)) {
-      case 8:
-        nccl_type = ncclDouble;
-        break;
-      case 4:
-        nccl_type = ncclFloat;
-        break;
-      case 2:
-        nccl_type = ncclHalf;
-        break;
-      default: throw_allreduce_exception("unsupported NCCL data type");
-    }
-
-    ncclRedOp_t nccl_redop;
-    switch(op) {
-      case ReductionOperator::sum:
-        nccl_redop = ncclSum;
-        break;
-      case ReductionOperator::prod:
-        nccl_redop = ncclProd;
-        break;
-      case ReductionOperator::min:
-        nccl_redop = ncclMin;
-        break;
-      case ReductionOperator::max:
-        nccl_redop = ncclMax;
-        break;
-      default:
-        throw_allreduce_exception("unsupported NCCL reduction operator");
-    }
-
+  static void NonblockingReduce(const T* sendbuf, T* recvbuf, size_t count,
+                                ReductionOperator op, int root, comm_type& comm,
+                                req_type& req, algo_type) {
+    ncclDataType_t nccl_type = internal::nccl::TypeMap<T>();
+    ncclRedOp_t nccl_redop = internal::nccl::ReductionOperator2ncclRedOp(op);
     if (sendbuf == internal::IN_PLACE<T>()) {
       sendbuf = recvbuf;
     }
-    
     comm.Reduce((void*) sendbuf, (void*) recvbuf, count,
                 nccl_type, nccl_redop, root, req);
   }
 
   template <typename T>
-  static void NonblockingReduce(
-      T* recvbuf, size_t count,
-      ReductionOperator op, 
-      int root, 
-      comm_type& comm,
-      req_type& req,
-      algo_type algo) {
-
+  static void NonblockingReduce(T* recvbuf, size_t count, ReductionOperator op,
+                                int root, comm_type& comm, req_type& req,
+                                algo_type algo) {
     NonblockingReduce(internal::IN_PLACE<T>(), recvbuf, count, op, root, comm,
                        req, algo);
   }
-
 
   template <typename T>
   static void Allgather(const T* sendbuf, T* recvbuf, size_t count,
@@ -266,50 +258,26 @@ class NCCLBackend {
   }
 
   template <typename T>
-  static void NonblockingAllgather(
-      const T* sendbuf, T* recvbuf, size_t count,
-      comm_type& comm,
-      req_type& req,
-      algo_type algo) {
-
-    ncclDataType_t nccl_type;
-    switch(sizeof(T)) {
-      case 8:
-        nccl_type = ncclDouble;
-        break;
-      case 4:
-        nccl_type = ncclFloat;
-        break;
-      case 2:
-        nccl_type = ncclHalf;
-        break;
-      default:
-        throw_allreduce_exception("unsupported NCCL data type");
-    }
-
+  static void NonblockingAllgather(const T* sendbuf, T* recvbuf, size_t count,
+                                   comm_type& comm, req_type& req, algo_type) {
+    ncclDataType_t nccl_type = internal::nccl::TypeMap<T>();
     if (sendbuf == internal::IN_PLACE<T>()) {
       sendbuf = recvbuf;
     }
-
     comm.Allgather((void*) sendbuf, (void*) recvbuf, count,
                    nccl_type, req);
   }
 
   template <typename T>
-  static void NonblockingAllgather(
-      T* recvbuf, size_t count,
-      comm_type& comm,
-      req_type& req,
-      algo_type algo) {
-  
+  static void NonblockingAllgather(T* recvbuf, size_t count, comm_type& comm,
+                                   req_type& req, algo_type algo) {
     NonblockingAllgather(internal::IN_PLACE<T>(),  recvbuf, count, comm, req, algo);
-
   }
 
   template <typename T>
   static void Reduce_scatter(const T* sendbuf, T* recvbuf, size_t recv_count,
-                        ReductionOperator op, comm_type& comm,
-                        algo_type algo) {
+                             ReductionOperator op, comm_type& comm,
+                             algo_type algo) {
     cudaStream_t default_stream = comm.get_default_stream();
     NonblockingReduce_scatter(sendbuf, recvbuf, recv_count, op, comm, default_stream, algo);
     comm.synchronize();
@@ -317,103 +285,44 @@ class NCCLBackend {
 
   template <typename T>
   static void Reduce_scatter(T* recvbuf, size_t recv_count,
-                        ReductionOperator op, comm_type& comm,
-                        algo_type algo) {
+                             ReductionOperator op, comm_type& comm,
+                             algo_type algo) {
     Reduce_scatter(internal::IN_PLACE<T>(), recvbuf, recv_count, op, comm, algo);
   }
 
   template <typename T>
-  static void NonblockingReduce_scatter(
-      const T* sendbuf, T* recvbuf, size_t recv_count,
-      ReductionOperator op,
-      comm_type& comm,
-      req_type& req,
-      algo_type algo) {
-
-    ncclDataType_t nccl_type;
-    switch(sizeof(T)) {
-      case 8:
-        nccl_type = ncclDouble;
-        break;
-      case 4:
-        nccl_type = ncclFloat;
-        break;
-      case 2:
-        nccl_type = ncclHalf;
-        break;
-      default:
-        throw_allreduce_exception("unsupported NCCL data type");
-    }
-
-    ncclRedOp_t nccl_redop;
-    switch(op) {
-      case ReductionOperator::sum:
-        nccl_redop = ncclSum;
-        break;
-      case ReductionOperator::prod:
-        nccl_redop = ncclProd;
-        break;
-      case ReductionOperator::min:
-        nccl_redop = ncclMin;
-        break;
-      case ReductionOperator::max:
-        nccl_redop = ncclMax;
-        break;
-      default:
-        throw_allreduce_exception("unsupported NCCL reduction operator");
-    }
-
+  static void NonblockingReduce_scatter(const T* sendbuf, T* recvbuf,
+                                        size_t recv_count, ReductionOperator op,
+                                        comm_type& comm, req_type& req,
+                                        algo_type) {
+    ncclDataType_t nccl_type = internal::nccl::TypeMap<T>();
+    ncclRedOp_t nccl_redop = internal::nccl::ReductionOperator2ncclRedOp(op);
     if (sendbuf == internal::IN_PLACE<T>()) {
       sendbuf = recvbuf;
     }
-    
     comm.Reduce_scatter((void*) sendbuf, (void*) recvbuf, recv_count,
-                   nccl_type, nccl_redop, req);
+                        nccl_type, nccl_redop, req);
   }
 
   template <typename T>
-  static void NonblockingReduce_scatter(
-      T* recvbuf, size_t recv_count,
-      ReductionOperator op, comm_type& comm,
-      req_type& req,
-      algo_type algo) {
+  static void NonblockingReduce_scatter(T* recvbuf, size_t recv_count,
+                                        ReductionOperator op, comm_type& comm,
+                                        req_type& req, algo_type algo) {
     NonblockingReduce_scatter(internal::IN_PLACE<T>(), recvbuf, recv_count, op, comm, req, algo);
   }
 
   template <typename T>
-  static void Bcast(const T* sendbuf, 
-                    size_t count, 
-                    int root, 
-                    comm_type& comm, 
+  static void Bcast(const T* sendbuf, size_t count, int root, comm_type& comm, 
                     algo_type algo) {
     cudaStream_t default_stream = comm.get_default_stream();
     NonblockingBcast(sendbuf, count, root, comm, default_stream, algo);
     comm.synchronize();
   }
 
-
   template <typename T>
-  static void NonblockingBcast(const T* sendbuf, 
-                               size_t count, 
-                               int root, 
-                               comm_type& comm,  
-                               req_type& req, 
-                               algo_type algo) {
-    ncclDataType_t nccl_type;
-    switch(sizeof(T)) {
-      case 8:
-        nccl_type = ncclDouble;
-        break;
-      case 4:
-        nccl_type = ncclFloat;
-        break;
-      case 2:
-        nccl_type = ncclHalf;
-        break;
-      default:
-        throw_allreduce_exception("unsupported NCCL data type");
-    }
-
+  static void NonblockingBcast(const T* sendbuf, size_t count, int root,
+                               comm_type& comm, req_type& req, algo_type) {
+    ncclDataType_t nccl_type = internal::nccl::TypeMap<T>();
     comm.Bcast((void *) sendbuf, count, nccl_type, root, req);
   }
 };
