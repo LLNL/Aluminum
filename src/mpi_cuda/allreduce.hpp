3001f8e836025ff66a4d778f857d9e721438cb93
Naoya Maruyama <maruyama3@llnl.gov>
2018-08-24 17:42:46 -0700

    Move MPICUDACommuniator to its own header and header files

--- src/mpi_cuda/allreduce.hpp
+++ src/mpi_cuda/allreduce.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "cuda.hpp"
-#include "cudacommunicator.hpp"
 #include "progress.hpp"
+#include "mpi_cuda/communicator.hpp"
 #include "mpi_cuda/allreduce_ring.hpp"
 #include "mpi_cuda/util.hpp"
 #include "mpi_impl.hpp"
@@ -11,22 +11,6 @@
 namespace Al {
 namespace internal {
 namespace mpi_cuda {
-
-class MPICUDACommunicator: public CUDACommunicator {
- public:
-  MPICUDACommunicator() : MPICUDACommunicator(MPI_COMM_WORLD, 0) {}
-  MPICUDACommunicator(cudaStream_t stream_) :
-    MPICUDACommunicator(MPI_COMM_WORLD, stream_) {}
-  MPICUDACommunicator(MPI_Comm comm_, cudaStream_t stream_) :
-    CUDACommunicator(comm_, stream_), m_ring(get_comm()) {}
-
-  RingMPICUDA &get_ring() {
-    return m_ring;
-  }
-
- protected:
-  RingMPICUDA m_ring;
-};
 
 template <typename T> inline
 void ring_allreduce(const T* sendbuf, T* recvbuf, size_t count,
