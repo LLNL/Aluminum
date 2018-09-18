1245a87f5de0c2afee1143b8ad599691ecd22f16
Nikoli Dryden <dryden1@llnl.gov>
2018-09-18 11:15:39 -0700

    Add a test to print support for stream memory operations.

---
+++ test/test_stream_mem_ops.cpp
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2018, Lawrence Livermore National Security, LLC.  Produced at the
+// Lawrence Livermore National Laboratory in collaboration with University of
+// Illinois Urbana-Champaign.
+//
+// Written by the LBANN Research Team (N. Dryden, N. Maruyama, et al.) listed in
+// the CONTRIBUTORS file. <lbann-dev@llnl.gov>
+//
+// LLNL-CODE-756777.
+// All rights reserved.
+//
+// This file is part of Aluminum GPU-aware Communication Library. For details, see
+// http://software.llnl.gov/Aluminum or https://github.com/LLNL/Aluminum.
+//
+// Licensed under the Apache License, Version 2.0 (the "Licensee"); you
+// may not use this file except in compliance with the License.  You may
+// obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the license.
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include "Al.hpp"
+
+int main(int argc, char** argv) {
+  cudaSetDevice(0);
+  Al::Initialize(argc, argv);
+  if (Al::internal::cuda::stream_memory_operations_supported()) {
+    std::cout << "Supported!" << std::endl;
+  } else {
+    std::cout << "Not supported! :(" << std::endl;
+  }
+  Al::Finalize();
+  return 0;
+}
