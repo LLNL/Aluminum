d162fd4c2a013e2bd9b15475ab4f75c265dc7672
Nikoli Dryden <dryden1@llnl.gov>
2018-09-18 14:19:39 -0700

    Remove unused AL_PE_SLEEPS tuning parameter.

--- src/tuning_params.hpp
+++ src/tuning_params.hpp
@@ -47,14 +47,6 @@
 #define AL_MPI_MULTITHREAD_BITWISE_THRESH 262144
 
 /**
- * Whether the progress engine just polls constantly or sleeps when there is no
- * work to do.
- * Sleeping will reduce unneeded CPU usage (probably helps if you're also doing
- * compute on the same core) but probably increases latency a little bit.
- */
-#define AL_PE_SLEEPS 0
-
-/**
  * Number of concurrent operations the progress engine will perform.
  * This must be a positive number.
  */
