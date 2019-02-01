c67ee561c67857626b216cf7b5eed5eb928e59da
Nikoli Dryden <dryden1@llnl.gov>
2019-02-01 11:50:41 -0800

    Add benchmarks for event and wait implementations.

---
+++ benchmark/benchmark_events.cpp
@@ -0,0 +1,93 @@
+/** Benchmark different event implementations. */
+
+#include <iostream>
+#include <cuda.h>
+#include "Al.hpp"
+#include "test_utils.hpp"
+#include "test_utils_cuda.hpp"
+#include "wait.hpp"
+
+class Event {
+public:
+  virtual void record(cudaStream_t stream) = 0;
+  virtual bool query() = 0;
+};
+
+class CudaEvent : public Event {
+public:
+  CudaEvent() {
+    cudaEventCreateWithFlags(&event, cudaEventDisableTiming);
+  }
+  ~CudaEvent() {
+    cudaEventDestroy(event);
+  }
+  void record(cudaStream_t stream) override {
+    cudaEventRecord(event, stream);
+  }
+  bool query() override {
+    return cudaEventQuery(event) == cudaSuccess;
+  }
+private:
+  cudaEvent_t event;
+};
+
+class CustomEvent : public Event {
+public:
+  CustomEvent() {
+    cudaMallocHost(&event, sizeof(int32_t));
+    __atomic_store_n(event, 1, __ATOMIC_SEQ_CST);
+    cuMemHostGetDevicePointer(
+      &dev_ptr, event, 0);
+  }
+  ~CustomEvent() {
+    cudaFreeHost(event);
+  }
+  void record(cudaStream_t stream) override {
+    __atomic_store_n(event, 0, __ATOMIC_SEQ_CST);
+    cuStreamWriteValue32(
+      stream, dev_ptr, 1, CU_STREAM_WRITE_VALUE_DEFAULT);
+  }
+  bool query() override {
+    return __atomic_load_n(event, __ATOMIC_SEQ_CST);
+  }
+private:
+  int32_t* event __attribute__((aligned(64)));
+  CUdeviceptr dev_ptr;
+};
+
+void do_benchmark(cudaStream_t stream, Event& event) {
+  const double wait_time = 0.0001;
+  std::vector<double> times, launch_times;
+  for (int i = 0; i < 100000; ++i) {
+    double launch_start = get_time();
+    gpu_wait(wait_time, stream);
+    event.record(stream);
+    double start = get_time();
+    while (!event.query()) {}
+    double end = get_time();
+    launch_times.push_back(start - launch_start);
+    times.push_back(end - start);
+    cudaStreamSynchronize(stream);
+  }
+  std::cout << "Launch: ";
+  print_stats(launch_times);
+  std::cout << "Query: ";
+  print_stats(times);
+}
+
+int main(int, char**) {
+  cudaSetDevice(0);
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
+  {
+    CudaEvent cuda_event;
+    CustomEvent custom_event;
+    std::cout << "Custom event:" << std::endl;
+    do_benchmark(stream, custom_event);
+    std::cout << "CUDA Event:" << std::endl;
+    do_benchmark(stream, cuda_event);
+  }
+  cudaStreamSynchronize(stream);
+  cudaStreamDestroy(stream);
+  return 0;
+}
---
+++ benchmark/benchmark_waits.cpp
@@ -0,0 +1,91 @@
+/** Benchmark different wait implementations. */
+
+#include <iostream>
+#include <cuda.h>
+#include "Al.hpp"
+#include "helper_kernels.hpp"
+#include "test_utils.hpp"
+#include "test_utils_cuda.hpp"
+#include "wait.hpp"
+
+class Wait {
+public:
+  Wait() {
+    cudaMallocHost(&wait_sync, sizeof(int32_t));
+    __atomic_store_n(wait_sync, 0, __ATOMIC_SEQ_CST);
+  }
+  ~Wait() {
+    cudaFreeHost(wait_sync);
+  }
+  virtual void wait(cudaStream_t stream) = 0;
+  virtual void signal() {
+    __atomic_store_n(wait_sync, 1, __ATOMIC_SEQ_CST);
+  }
+
+  int32_t* wait_sync __attribute__((aligned(64)));
+};
+
+class StreamOpWait : public Wait {
+public:
+  StreamOpWait() : Wait() {
+    cuMemHostGetDevicePointer(&dev_ptr, wait_sync, 0);
+  }
+  ~StreamOpWait() {}
+  void wait(cudaStream_t stream) override {
+    Al::internal::cuda::launch_wait_kernel(stream, 1, dev_ptr);
+  }
+  CUdeviceptr dev_ptr;
+};
+
+class KernelWait : public Wait {
+public:
+  KernelWait() : Wait() {
+    cudaHostGetDevicePointer(&dev_ptr, wait_sync, 0);
+  }
+  ~KernelWait() {}
+  void wait(cudaStream_t stream) override {
+    Al::internal::cuda::launch_wait_kernel(stream, 1, dev_ptr);
+  }
+  int32_t* dev_ptr __attribute__((aligned(64)));
+};
+
+void do_benchmark(cudaStream_t stream, Wait& wait) {
+  cudaEvent_t e;
+  cudaEventCreateWithFlags(&e, cudaEventDisableTiming);
+  std::vector<double> times, launch_times;
+  for (int i = 0; i < 100000; ++i) {
+    double launch_start = get_time();
+    wait.wait(stream);
+    double launch_end = get_time();
+    cudaEventRecord(e, stream);
+    double start = get_time();
+    wait.signal();
+    while (cudaEventQuery(e) == cudaErrorNotReady) {}
+    double end = get_time();
+    launch_times.push_back(launch_end - launch_start);
+    times.push_back(end - start);
+    cudaStreamSynchronize(stream);
+  }
+  std::cout << "Launch: ";
+  print_stats(launch_times);
+  std::cout << "Signal: ";
+  print_stats(times);
+  cudaEventDestroy(e);
+}
+
+int main(int, char**) {
+  cudaSetDevice(0);
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
+  {
+    StreamOpWait stream_op_wait;
+    KernelWait kernel_wait;
+    std::cout << "StreamOp wait:" << std::endl;
+    do_benchmark(stream, stream_op_wait);
+    std::cout << "Kernel wait:" << std::endl;
+    do_benchmark(stream, kernel_wait);
+  }
+  cudaStreamSynchronize(stream);
+  cudaStreamDestroy(stream);
+  return 0;
+}
