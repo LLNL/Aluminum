727e46136ad4643788bb4b8d6e2da767337b7810
Thomas R. Benson <benson31@llnl.gov>
2023-01-19 15:28:28 -0800

    A little tidying

--- src/cuda/gpu_wait.cpp
+++ src/cuda/gpu_wait.cpp
@@ -63,11 +63,7 @@ GPUWait::~GPUWait() {
 
 void GPUWait::wait(AlGpuStream_t stream) {
   if (stream_memory_operations_supported()) {
-#ifdef AL_HAS_ROCM
-    launch_wait_kernel(stream, 1, static_cast<int32_t *>(wait_sync_dev_ptr));
-#else
     launch_wait_kernel(stream, 1, wait_sync_dev_ptr);
-#endif
   } else {
     launch_wait_kernel(stream, 1, wait_sync_dev_ptr_no_stream_mem_ops);
   }
